5553c268bfa1a5c256fa6e715a3f399690acf3bd
informationtoyou <pandeyatharva13@gmail.com>
2025-01-02 19:23:52 +0100

    Commit

--- main.cpp
+++ main.cpp
@@ -3,4 +3,8 @@ using namespace std;
 
 int main() {
 	cout << "Welcome to Lightweight Calculator!" << endl;
+	cout << "This basic calculator can support digits upto 20 digits at least." << endl;
+
+	cout << "Lightweight Calculator is licenced under the MIT Licence 2025 onwards." << endl;
+
 }
