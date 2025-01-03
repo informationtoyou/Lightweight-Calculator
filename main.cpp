1de7ddc0fb83de24affe56ba7b6c120cb6e8402b
informationtoyou <pandeyatharva13@gmail.com>
2025-01-03 18:46:55 +0100

    Added conversion from imperial to metric and vise versa for volume, weight, temperature, and length.

--- main.cpp
+++ main.cpp
@@ -117,27 +117,99 @@ string divideLargeNumbers(const string& num1, const string& num2, int precision)
     return result.empty() ? "0" : result;
 }
 
+// Conversion Functions
+void convertWeight() {
+    double value;
+    string unit;
+    cout << "Enter the weight value: ";
+    cin >> value;
+    cout << "Enter the unit (kg, lb): ";
+    cin >> unit;
+
+    if (unit == "kg") {
+        cout << value << " kg = " << value * 2.20462 << " lb" << endl;
+    }
+    else if (unit == "lb") {
+        cout << value << " lb = " << value / 2.20462 << " kg" << endl;
+    }
+    else {
+        cout << "Invalid unit!" << endl;
+    }
+}
+
+void convertTemperature() {
+    double value;
+    string unit;
+    cout << "Enter the temperature value: ";
+    cin >> value;
+    cout << "Enter the unit (C, F): ";
+    cin >> unit;
+
+    if (unit == "C") {
+        cout << value << " °C = " << (value * 9 / 5) + 32 << " °F" << endl;
+    }
+    else if (unit == "F") {
+        cout << value << " °F = " << (value - 32) * 5 / 9 << " °C" << endl;
+    }
+    else {
+        cout << "Invalid unit!" << endl;
+    }
+}
+
+void convertLength() {
+    double value;
+    string unit;
+    cout << "Enter the length value: ";
+    cin >> value;
+    cout << "Enter the unit (m, ft): ";
+    cin >> unit;
+
+    if (unit == "m") {
+        cout << value << " m = " << value * 3.28084 << " ft" << endl;
+    }
+    else if (unit == "ft") {
+        cout << value << " ft = " << value / 3.28084 << " m" << endl;
+    }
+    else {
+        cout << "Invalid unit!" << endl;
+    }
+}
+
+void convertVolume() {
+    double value;
+    string unit;
+    cout << "Enter the volume value: ";
+    cin >> value;
+    cout << "Enter the unit (L, gal): ";
+    cin >> unit;
+
+    if (unit == "L") {
+        cout << value << " L = " << value * 0.264172 << " gal" << endl;
+    }
+    else if (unit == "gal") {
+        cout << value << " gal = " << value / 0.264172 << " L" << endl;
+    }
+    else {
+        cout << "Invalid unit!" << endl;
+    }
+}
+
 // Main calculator loop
 int main() {
     string number1, number2;
     char operation;
-    int precision = 3; // Default precision for division - feel free to change in your version
+    int precision = 3; // Default precision for division
 
     cout << "Welcome to Lightweight Calculator!" << endl;
     cout << "This calculator was made by informationtoyou and is freely available on GitHub and is under an MIT License" << endl;
 
     while (true) {
-        cout << "\nEnter the first number: ";
-        cin >> number1;
-        cout << "Enter the second number: ";
-        cin >> number2;
-
-        cout << "Choose an operation:" << endl;
-        cout << "A - Addition" << endl;
-        cout << "S - Subtraction" << endl;
-        cout << "M - Multiplication" << endl;
-        cout << "D - Division" << endl;
-        cout << "P - Set Precision for Division" << endl;
+        cout << "\nChoose an option:" << endl;
+        cout << "C - Calculator" << endl;
+        cout << "T - Convert Temperature" << endl;
+        cout << "W - Convert Weight" << endl;
+        cout << "L - Convert Length" << endl;
+        cout << "V - Convert Volume" << endl;
         cout << "Q - Quit" << endl;
         cout << "Your choice: ";
         cin >> operation;
@@ -148,36 +220,75 @@ int main() {
         }
 
         switch (operation) {
-        case 'A':
-        case 'a':
-            cout << "Result: " << addLargeNumbers(number1, number2) << endl;
-            break;
-        case 'S':
-        case 's':
-            if (number1 >= number2)
-                cout << "Result: " << subtractLargeNumbers(number1, number2) << endl;
-            else
-                cout << "Result: -" << subtractLargeNumbers(number2, number1) << endl;
-            break;
-        case 'M':
-        case 'm':
-            cout << "Result: " << multiplyLargeNumbers(number1, number2) << endl;
-            break;
-        case 'D':
-        case 'd':
-            cout << "Result: " << divideLargeNumbers(number1, number2, precision) << endl;
-            break;
-        case 'P':
-        case 'p':
-            cout << "Enter the number of decimal places for division: ";
-            cin >> precision;
-            if (precision < 0) {
-                cout << "Precision must be non-negative. Setting to default 3." << endl;
-                precision = 3;
+        case 'C':
+        case 'c':
+            cout << "\nEnter the first number: ";
+            cin >> number1;
+            cout << "Enter the second number: ";
+            cin >> number2;
+
+            cout << "Choose an operation:" << endl;
+            cout << "A - Addition" << endl;
+            cout << "S - Subtraction" << endl;
+            cout << "M - Multiplication" << endl;
+            cout << "D - Division" << endl;
+            cout << "P - Set Precision for Division" << endl;
+            cout << "Your choice: ";
+            cin >> operation;
+
+            switch (operation) {
+            case 'A':
+            case 'a':
+                cout << "Result: " << addLargeNumbers(number1, number2) << endl;
+                break;
+            case 'S':
+            case 's':
+                if (number1 >= number2)
+                    cout << "Result: " << subtractLargeNumbers(number1, number2) << endl;
+                else
+                    cout << "Result: -" << subtractLargeNumbers(number2, number1) << endl;
+                break;
+            case 'M':
+            case 'm':
+                cout << "Result: " << multiplyLargeNumbers(number1, number2) << endl;
+                break;
+            case 'D':
+            case 'd':
+                cout << "Result: " << divideLargeNumbers(number1, number2, precision) << endl;
+                break;
+            case 'P':
+            case 'p':
+                cout << "Enter the number of decimal places for division: ";
+                cin >> precision;
+                if (precision < 0) {
+                    cout << "Precision must be non-negative. Setting to default 3." << endl;
+                    precision = 3;
+                }
+                break;
+            default:
+                cout << "Invalid operation. Try again." << endl;
             }
             break;
+
+        case 'T':
+        case 't':
+            convertTemperature();
+            break;
+        case 'W':
+        case 'w':
+            convertWeight();
+            break;
+        case 'L':
+        case 'l':
+            convertLength();
+            break;
+        case 'V':
+        case 'v':
+            convertVolume();
+            break;
+
         default:
-            cout << "Invalid operation. Try again." << endl;
+            cout << "Invalid choice. Try again." << endl;
         }
     }
 
